7716daa1ef7e2c2954051992faa98c7f706762b2
SeasonLei <30384084@qq.com>
2022-06-27 18:36:28 +0800

    Miner:add about seascape

--- migrations/2_nft_migration.js
+++ migrations/2_nft_migration.js
@@ -1,8 +1,35 @@
 var MineNFT = artifacts.require("./MineNFT.sol");
+var Seascape   = require("seascape");
 
 module.exports = async function(deployer, network) {
    
         await deployer.deploy(MineNFT).then(function(){
             console.log("MineNFT contract was deployed at address: "+ MineNFT.address);
         });
+
+    //      let truffleParams = {
+    //     projectName: 'season',
+    //     projectEnv: 'beta',
+
+    //     contractName: 'MineNFT',
+    //     contractType: 'main',
+    //     contractAbi: MineNFT.abi,
+    //     contractAddress: MineNFT.address,
+        
+    //     networkId: await deployer.network_id,
+    //     txid: MineNFT.transactionHash,
+    //     owner: deployer.address,
+        
+    //     // verifier: "", optionally
+    //     // fund: "", optioanlly
+    // };
+
+    // let cdnUpdated = await Seascape.CdnWrite.setTruffleSmartcontract(truffleParams);
+  
+    // if (cdnUpdated) {
+    //     console.log(`CDN was updated successfully`);
+    // } else {
+    //     console.log(`CDN update failed. Please upload it manually!`);
+    //     console.log(truffleParams);
+    // }
 };
