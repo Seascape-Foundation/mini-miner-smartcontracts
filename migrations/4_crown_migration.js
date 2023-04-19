a8bdb232bf4086eeef3781e21da30cdae97b6760
ahmetson <ahmetson@zoho.com>
2023-04-19 16:17:31 +0300

    Delete crowns and mscp

--- migrations/4_crown_migration.js
+++
@@ -1,7 +0,0 @@
-var Crowns = artifacts.require("./CrownsToken.sol");
-
-module.exports = function(deployer, network) {
-    deployer.deploy(Crowns).then(function(){
-	    console.log("Crowns was deployed at: "+Crowns.address);
-    });
-}
--- migrations/5_mscp_migration.js
+++
@@ -1,7 +0,0 @@
-var Mscp = artifacts.require("./MscpToken.sol");
-
-module.exports = function(deployer, network) {
-    deployer.deploy(Mscp).then(function(){
-	    console.log("Mscp was deployed at: "+ Mscp.address);
-    });
-}
